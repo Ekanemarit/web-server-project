1a2fe6ed6b4f0959eb2a8ec5bbdd983eb6030803
Ekanemarit <ladyekanem@gmail.com>
2023-07-19 16:24:20 -0400

    changed the bucket name

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ provider "aws" {
 
 
 resource "aws_s3_bucket" "example" {
-  bucket = "my-tf-test-bucket"
+  bucket = "mdsfdsfsdf-buckedfsdfsdfdsfdsfsdfft"
 
   tags = {
     Name        = "My bucket"
